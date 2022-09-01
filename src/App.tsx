22eaafeacd8ad020fb7ab719f9d9743bc3ddd7d6
ShakhawatHossain13 <72038014+ShakhawatHossain13@users.noreply.github.com>
2022-09-01 10:33:45 +0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';  
+import { Routes, Route } from "react-router-dom";  
+const App: React.FC=()=> {
+
+  return (  
+    <React.Fragment> 
+       
+      <Routes> 
+          
+      </Routes>    
+    </React.Fragment>
+  );
+}
+
+export default App;
