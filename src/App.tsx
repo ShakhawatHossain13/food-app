9c0bf1a069666ce2b7cce82de0993c861f899a8f
Tasnuba Sraboni <62016183+tasnuba-sraboni@users.noreply.github.com>
2022-09-01 16:36:20 +0600

    database modification v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import UseFirebase from "./database/firebaseConfig";
+import { CRUD } from "./database/crud";
 
 const App: React.FC = () => {
   return (
     <React.Fragment>
+      <CRUD />
       <h1>Hello</h1>
       <h1>Hello From Imriaz</h1>
       <h1>Hello From Tasnuba</h1>
       <h1>Hello from mernOJT</h1>
       <h1>Hello from Tasnuba-mernOJT</h1>
       <h1>Hello From Shaks</h1>
-      <UseFirebase />
     </React.Fragment>
   );
 };
--- src/database/firebaseConfig/index.ts
+++ src/database/firebaseConfig/index.ts
@@ -1,13 +1,9 @@
 
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getFirestore} from "firebase/firestore";
 
-type CRUDProps = {
-    title: string;
-    description: string;
-  };
 
-const UseFirebase = async() => {
+
   const firebaseConfig = {
     apiKey: "AIzaSyDWztXPDt_fIjY_n5saKRTROLN3kxik2HA",
     authDomain: "food-app-6978f.firebaseapp.com",
@@ -18,37 +14,8 @@ const UseFirebase = async() => {
     appId: "1:271724303724:web:b213e48aa2e157bf804f49",
   };
 
-  initializeApp(firebaseConfig);
-  const db = getFirestore();
-
-  //coolection ref
-  const colRef = collection(db, "blog");
-
-  // get collection data
-
-  try{
-    const result = await getDocs(colRef);
-
-    const prepareData = result?.docs.map((item) => {
-      let temp = item.data();
-      let obj: CRUDProps = {      
-        title: temp.title,
-      description: temp.description
-        }
-        return obj;
-      })
-      return prepareData;
-    }catch (error) {
-  console.log(error);
-}
-  
-
-}
-
-
-
-export default UseFirebase;
-
+  const app = initializeApp(firebaseConfig);
+export const firebaseDatabase = getFirestore(app);
 
 
 
