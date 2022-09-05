b2eb773f102ad8a13ddc44cd591f415f08e3b7e8
Tasnuba Sraboni <62016183+tasnuba-sraboni@users.noreply.github.com>
2022-09-05 12:54:02 +0600

    added blog details and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
 import HomePage from "./components/HomePage";
+import BlogDetails from "./components/HomePage/BlogDetails";
+import MenuBar from "./components/MenuBar";
 
 const App: React.FC = () => {
-
   return (
-    <React.Fragment>   
-      <HomePage />   
+    <React.Fragment>
+      {/* <HomePage /> */}
+      <MenuBar />
+      <Routes>
+        <Route path="/blogdetails" element={<BlogDetails />} />
+      </Routes>
+      <Footer />
     </React.Fragment>
   );
 };
 
 export default App;
-
-
-
 
 // import { getData } from "./database/crud";
 // import Blog from "./database/crud";
---
+++ src/components/HomePage/BlogDetails/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { images } from "./blogdetailsimages";
+import "./style.css";
+
+const BlogDetails: React.FC = () => {
+  const [selected, setSelected] = React.useState(images[0].bannerImage);
+  return (
+    <React.Fragment>
+      <div className="blogdetails">
+        <div className="blogdetails__image">
+          <div className="blogdetails__image__main">
+            <img
+              src={selected}
+              className="blogdetails__image__main--selected"
+              alt="selected"
+            />
+          </div>
+          <div className="blogdetails__image__sub">
+            {images.map((img) => (
+              <img
+                style={{
+                  border:
+                    selected === img.bannerImage ? "2px solid cadetblue" : "",
+                }}
+                src={img.bannerImage}
+                alt="Blog Image"
+                onClick={() => setSelected(img.bannerImage)}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="blogdetails__image" style={{ textAlign: "start" }}>
+          <h3>{images[0].title}</h3>
+          <p>{images[0].date}</p>
+          <p>{images[0].description}</p>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default BlogDetails;
