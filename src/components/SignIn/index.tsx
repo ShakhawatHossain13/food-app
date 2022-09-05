2047d79d5e080fee94487d47cbebea4f4a0d257d
Md. Imriaz Uddin <imriaz.uddin@bjitgroup.com>
2022-09-05 16:10:08 +0600

    Sign in Page added

---
+++ src/components/SignIn/index.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "./style.css";
+import homeslider from "./home_slider.png";
+import logo from "../../images/logo.png";
+
+const SignIn: React.FC = () => {
+  return (
+    <React.Fragment>
+      <section
+        className="signIn__slider"
+        style={{ background: `url(${homeslider}) ` }}
+      >
+        <div className="signIn__slider__row">
+          <div className="signIn__slider__row__main">
+            <img
+              className="signIn__slider__row__main__logo"
+              src={logo}
+              alt="Red Onion Logo"
+            />
+            <form className="signIn__slider__row__main__form">
+              <input
+                type="email"
+                className="signIn__slider__row__main__form__input"
+                id="email"
+                name="email"
+                placeholder="Email"
+              />
+              <input
+                type="password"
+                className="signIn__slider__row__main__form__input"
+                id="password"
+                name="password"
+                placeholder="Password"
+              />
+              <input
+                type="submit"
+                className="signIn__slider__row__main__form__input"
+                id="submit"
+                value="Sign in"
+              />
+            </form>
+            <p className="signIn__slider__row__main__form__link">
+              Don't have an account? <a href="/signup">Click here...</a>
+            </p>
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
+  );
+};
+
+export default SignIn;
--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -1,56 +1,64 @@
-import React from "react"; 
-import './style.css';
-import homeslider from './home_slider.png';
+import React from "react";
+import "./style.css";
+import homeslider from "./home_slider.png";
 import logo from "../../images/logo.png";
 
 const SignUp: React.FC = () => {
   return (
     <React.Fragment>
-        <section className="signup__slider" style={{ background: `url(${homeslider}) `  }} >
-            <div className="signup__slider__row">
-                <div className="signup__slider__row__main"> 
-                    <img className="signup__slider__row__main__logo" src={logo} alt="Red Onion Logo"  />                 
-                    <form 
-                      className="signup__slider__row__main__form">
-                      <input 
-                        type="text"
-                        className="signup__slider__row__main__form__input"
-                        id="name"
-                        name="name"
-                        placeholder="Name"
-                      />
-                      <input 
-                        type="email"
-                        className="signup__slider__row__main__form__input"
-                        id="email"
-                        name="email"
-                        placeholder="Email"
-                      />
-                      <input 
-                        type="password"
-                        className="signup__slider__row__main__form__input"
-                        id="password"
-                        name="password"
-                        placeholder="Password"
-                      />
-                      <input 
-                        type="password"
-                        className="signup__slider__row__main__form__input"
-                        id="confirmPassword"
-                        name="confirmPassword"
-                        placeholder="Confirm Password"
-                      />
-                       <input 
-                        type="submit"
-                        className="signup__slider__row__main__form__input"
-                        id="submit"
-                        value="Sign Up"
-                      />
-                    </form>  
-                    <p className="signup__slider__row__main__form__link">Already have an account? <a href="#">Click here...</a></p>
-                </div>
-            </div>
-        </section>     
+      <section
+        className="signup__slider"
+        style={{ background: `url(${homeslider}) ` }}
+      >
+        <div className="signup__slider__row">
+          <div className="signup__slider__row__main">
+            <img
+              className="signup__slider__row__main__logo"
+              src={logo}
+              alt="Red Onion Logo"
+            />
+            <form className="signup__slider__row__main__form">
+              <input
+                type="text"
+                className="signup__slider__row__main__form__input"
+                id="name"
+                name="name"
+                placeholder="Name"
+              />
+              <input
+                type="email"
+                className="signup__slider__row__main__form__input"
+                id="email"
+                name="email"
+                placeholder="Email"
+              />
+              <input
+                type="password"
+                className="signup__slider__row__main__form__input"
+                id="password"
+                name="password"
+                placeholder="Password"
+              />
+              <input
+                type="password"
+                className="signup__slider__row__main__form__input"
+                id="confirmPassword"
+                name="confirmPassword"
+                placeholder="Confirm Password"
+              />
+              <input
+                type="submit"
+                className="signup__slider__row__main__form__input"
+                id="submit"
+                value="Sign Up"
+              />
+            </form>
+            <p className="signup__slider__row__main__form__link">
+              Already have an account? <a href="/signin">Click here...</a>
+            </p>
+          </div>
+        </div>
+      </section>
     </React.Fragment>
   );
 };
