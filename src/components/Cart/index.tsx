aabbd94df556f3f042c7679b4d370b7246aa2598
ShakhawatHossain13 <72038014+ShakhawatHossain13@users.noreply.github.com>
2022-09-05 17:25:44 +0600

    cart background

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -5,7 +5,7 @@ import homeslider from "./home_slider.png";
 const Cart: React.FC = () => {
   return (
     <React.Fragment> 
-      <div className="cart" style={{ background: `url(${background})`}}>      
+      <div className="cart" style={{ background: `url(${homeslider})`}}>      
         <div className="cart__row">
            <h1>Hello</h1>
         </div>         
