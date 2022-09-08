1518ecaf9c127cb567e868b85343626e811b3cf9
Tasnuba Sraboni <62016183+tasnuba-sraboni@users.noreply.github.com>
2022-09-08 08:55:10 +0600

    Firebase new config

--- src/database/firebaseConfig/index.ts
+++ src/database/firebaseConfig/index.ts
@@ -3,16 +3,26 @@ import { initializeApp } from "firebase/app";
 import { getFirestore} from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage} from "firebase/storage";
-  const firebaseConfig = {
-    apiKey: "AIzaSyDWztXPDt_fIjY_n5saKRTROLN3kxik2HA",
-    authDomain: "food-app-6978f.firebaseapp.com",
-    databaseURL: "https://food-app-6978f-default-rtdb.firebaseio.com",
-    projectId: "food-app-6978f",
-    storageBucket: "food-app-6978f.appspot.com",
-    messagingSenderId: "271724303724",
-    appId: "1:271724303724:web:b213e48aa2e157bf804f49",
-  };
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyDWztXPDt_fIjY_n5saKRTROLN3kxik2HA",
+  //   authDomain: "food-app-6978f.firebaseapp.com",
+  //   databaseURL: "https://food-app-6978f-default-rtdb.firebaseio.com",
+  //   projectId: "food-app-6978f",
+  //   storageBucket: "food-app-6978f.appspot.com",
+  //   messagingSenderId: "271724303724",
+  //   appId: "1:271724303724:web:b213e48aa2e157bf804f49",
+  // };
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8TRZbNau2aZIn-9-8BSVFBeNx7uCY6IE",
+  authDomain: "food-app-78d24.firebaseapp.com",
+  databaseURL: "https://food-app-78d24.firebaseio.com",
+  projectId: "food-app-78d24",
+  storageBucket: "food-app-78d24.appspot.com",
+  messagingSenderId: "490475426385",
+  appId: "1:490475426385:web:e63effd07fbc9ef1c31e70"
+};
   const app = initializeApp(firebaseConfig);
 
   const firebaseDatabase = getFirestore(app);
