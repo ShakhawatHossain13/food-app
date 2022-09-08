3ca41d4ad4d8bb97737d7153cd94ccca0384ae70
Md. Imriaz Uddin <imriaz.uddin@bjitgroup.com>
2022-09-08 14:48:14 +0600

    Multiple image upload

--- src/components/Dashboard/MultipleImageUpload/index.tsx
+++ src/components/Dashboard/MultipleImageUpload/index.tsx
@@ -1,22 +1,34 @@
 import React from "react";
 import "./style.css";
 import { FaCheck } from "react-icons/fa";
+import { storage } from "../../../database/firebaseConfig";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 
 const MultipleImageUpload: React.FC = () => {
-  const [images, setImages] = React.useState<string[]>([]);
-  const [selected, setSelected] = React.useState(images[0]);
+  const [images, setImages] = React.useState([]);
+  const [displayImages, setDisplayImages] = React.useState<string[]>([]);
+  const [selected, setSelected] = React.useState(displayImages[0]);
+      const [imgUrls, setImgUrls] = React.useState([]);
+      const [progress, setProgress] = React.useState<number>(0);
 
-  const imageHandleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const imageHandleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
       const fileArray = Array.from(e.target.files).map((file) =>
         URL.createObjectURL(file)
       );
-      setImages(fileArray);
+      setDisplayImages(fileArray);
     }
   };
-  
+      const handleChange = (e: any) => {
+        for (let i = 0; i < e.target.files.length; i++) {
+          const newImage = e.target.files[i];
+          newImage["id"] = Math.random();
+          setImages((prevState): any => [...prevState, newImage]);
+        }
+      };
+
   const renderImages = () => {
-    return images.map((photo) => {
+    return displayImages.map((photo) => {
       return (
         <>
           <img
@@ -35,14 +47,61 @@ const MultipleImageUpload: React.FC = () => {
       );
     });
   };
+
+    const handleUpload = () => {
+      const promises: any = [];
+      images.map((image) => {
+        const storageRef = ref(storage, `/images/${Math.random()}`);
+        const uploadTask: any = uploadBytesResumable(storageRef, image);
+        promises.push(uploadTask);
+        uploadTask.on(
+          "state_changed",
+          (snapshot: any) => {
+            const progress = Math.round(
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+            );
+            setProgress(progress);
+          },
+          (error: any) => {
+            console.log(error);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              setImgUrls((prevState): any => [...prevState, downloadURL]);
+            });
+          }
+        );
+      });
+
+      Promise.all(promises)
+        .then(() => alert("All images uploaded"))
+        .catch((err) => console.log(err));
+    };
+
+    console.log("images: ", images);
+    console.log("urls", imgUrls);
+
   return (
     <React.Fragment>
       <div className="image">
         <div>
-          <input type="file" multiple onChange={imageHandleChange} />
+          <input
+            type="file"
+            id="image"
+            name="image"
+            multiple
+            onChange={(e)=>{
+                imageHandleChange(e);
+                handleChange(e);
+            }}
+          />
         </div>
 
         <div className="image__preview">{renderImages()}</div>
+        <button onClick={handleUpload} type="submit">
+          Upload
+        </button>
+        <span>{progress} %</span>
       </div>
     </React.Fragment>
   );
