da33f15a66d84c75705c46dd610a8cf38bf4793d
ShakhawatHossain13 <72038014+ShakhawatHossain13@users.noreply.github.com>
2022-09-22 10:48:19 +0600

    Product details page responsive v_1

--- src/components/ProductsDetails/ProductsDetailsBottom/index.tsx
+++ src/components/ProductsDetails/ProductsDetailsBottom/index.tsx
@@ -27,6 +27,32 @@ const ProductsDetailsBottom: React.FC<ProductsDetailsBottomProps> = ({
     slidesToShow: 3,
     slidesToScroll: 1,
     cssEase: "liner",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
   return (
     <div style={{ marginBottom: "50px" }}>
